9740eef7d082b86c6960890ddf9de58eacb03734
chen3yym <40581631+chen3yym@users.noreply.github.com>
2022-12-18 02:44:46 -0600

    add basic view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0)
+                        .stroke(lineWidth: 3)
+            Text("Hello, world!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)//default color
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
